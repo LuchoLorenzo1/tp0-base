0c8cbe98ed83944c7c3972bb7eb3fa8c56d4788e
LuchoLorenzo1 <LuchoLorenzo1@users.noreply.github.com>
2024-08-31 12:52:26 -0300

    agrego script validar-echo-server

---
+++ validar-echo-server.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+SERVER_CONTAINER_NAME="server"
+NETWORK_NAME="tp0_testing_net"
+
+SERVER_PORT=9999
+MESSAGE="message"
+
+RESPONSE=$(docker run --rm --network $NETWORK_NAME busybox sh -c "echo $MESSAGE | nc $SERVER_CONTAINER_NAME $SERVER_PORT")
+
+if [ "$RESPONSE" == "$MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
